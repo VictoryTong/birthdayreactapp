a6eaa47027ec5166575a89ec78e9bd5a22095216
Tongbram <victory.tongbram@hpe.com>
2024-06-22 15:43:23 +0530

    New files

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+  return (<><Nav><NavMenu><NavLink to="/about" activeStyle></NavLink></NavMenu></Nav>);
+};
